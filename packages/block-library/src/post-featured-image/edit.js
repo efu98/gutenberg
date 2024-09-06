e45ce6cab318408a6b8b11878aa03b7dc452e7ea
Elise Fu <elisefu@hotmail.com>
2024-09-06 07:21:32 +0200

    Revert "Add toggle on featured image to fallback to first image"

--- packages/block-library/src/post-featured-image/edit.js
+++ packages/block-library/src/post-featured-image/edit.js
@@ -239,14 +239,6 @@ export default function PostFeaturedImageEdit( {
 							/>
 						</>
 					) }
-					<ToggleControl
-						__nextHasNoMarginBottom
-						label={ __( 'Fallback to first image from post' ) }
-						onChange={ ( value ) =>
-							setAttributes( { useFirstImageFromPost: value } )
-						}
-						checked={ useFirstImageFromPost }
-					/>
 				</PanelBody>
 			</InspectorControls>
 		</>
